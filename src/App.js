2cddd092289531bd1832378ebff3f4543d462400
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-04-27 15:17:47 +0800

    initial login no implementations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Signup />
+        <Login />
       </header>
     </div>
   );
---
+++ src/pages/Login.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import './css/Signup.css'; 
+
+function Login() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    setFormData({
+      firstName: '',
+      lastName: '',
+      username: '',
+      password: ''
+    });
+  };
+
+  return (
+    <div className="signup-container">
+      <div className="image-container">
+        <div className="image">
+          <img src='logo.png' alt="Snake" className="snake-image" />
+        </div>
+        <div className="logo">
+          <img src='logo1.png' alt="Snake" className="snake-word" />
+        </div>
+      </div>
+      <div className="signup-form">
+        <p className='signupname'>LOGIN</p>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              placeholder="Username"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Password" 
+              required
+            />
+          </div>
+          <p className="login-text">Doesn't have an account? <span style={{ textDecoration: 'underline' }}>Sign Up</span></p>
+          <button type="submit">LOGIN</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
