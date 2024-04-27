eb1ea663a56c25fe483f3ce1cddcb2c8cd513099
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-04-27 22:26:59 +0800

    changed bgcolor for signup and login

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
-// import React from 'react';
-// import Signup from './pages/Signup';
-// import { BrowserRouter,Routes, Route } from 'react-router-dom';
-// import Login from './pages/Login';
-// import Dashboard from './pages/Dashboard';
-
-
-// function App() {
-//   return (
-//     // <BrowserRouter>
-//     // <Routes>
-//     //   <Route path="/" element={<Login />} />
-//     //   <Route path="/signup" element={<Signup />} />
-//     // </Routes>
-//     // </BrowserRouter>
-//     <>
-//     <Dashboard />
-//     </>
-//   );
-// }
-
-// export default App;
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SnakeGameBoard from './pages/SnakeGameBoard';
+import Dashboard from './pages/Dashboard';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
-  // Use either ReactDOM.render() or ReactDOM.createRoot(), not both
-  // In this example, I'll use ReactDOM.render()
-  // Make sure to replace 'root' with the ID of your container element
   return (
-    <React.StrictMode>
-      <SnakeGameBoard />
-    </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/snake-game" element={<SnakeGameBoard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
