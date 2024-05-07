b6bfc71a5492bb2470358d6011c725d9075fdb67
ritchelmendaros <ritchel.mendaros@cit.edu>
2024-05-07 22:48:07 +0800

    implemented leaderboard

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,14 +3,41 @@ import React from "react";
 function GameOver(props) {
   return (
     <>
+      <div className="leaderboard-container">
+        <h2>Leaderboard</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Score</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.leaderboard.map((entry, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{entry.score}</td>
+                <td>{`${entry.user.firstname} ${entry.user.lastname}`}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <div className="score-container">
         <div className="score">
-          <span style={{ color: "white", fontWeight:"bold" }}>Score:</span>{" "}
-          <span style={{ color: "blue", fontWeight:"bold" }}>{props.score}</span>
+          <span style={{ color: "white", fontWeight: "bold" }}>Score:</span>{" "}
+          <span style={{ color: "blue", fontWeight: "bold" }}>
+            {props.score}
+          </span>
         </div>
         <div className="score">
-          <span style={{ color: "white", fontWeight:"bold" }}>Highest Score:</span>{" "}
-          <span style={{ color: "red", fontWeight:"bold" }}>{props.highScore}</span>
+          <span style={{ color: "white", fontWeight: "bold" }}>
+            Highest Score:
+          </span>{" "}
+          <span style={{ color: "red", fontWeight: "bold" }}>
+            {props.highScore}
+          </span>
         </div>
       </div>
 
@@ -19,7 +46,6 @@ function GameOver(props) {
         style={{
           width: props.width,
           height: props.height,
-          borderRadius: '5px',
           borderWidth: props.width / 50,
         }}
       >
--- src/pages/SnakeGameBoard.js
+++ src/pages/SnakeGameBoard.js
@@ -214,9 +214,10 @@ class SnakeGameBoard extends React.Component {
         // Fetch data
         this.fetchSnakeColor();
         this.fetchAppleColor();
-        
+
         // update score
         this.updateScore();
+        this.fetchLeaderboard();
       }
     );
   }
@@ -509,7 +510,9 @@ class SnakeGameBoard extends React.Component {
   updateScore() {
     axios
       .put(`http://localhost:8080/scoreboard/updateScore`, {
-        userid: this.props.userId,
+        user: {
+          userid: this.props.userId,
+        },
         score: this.state.score,
       })
       .then((response) => {
@@ -545,12 +548,33 @@ class SnakeGameBoard extends React.Component {
           highScore={this.state.highScore}
           newHighScore={this.state.newHighScore}
           score={this.state.score}
+          leaderboard={this.state.leaderboard}
         />
       );
     }
-
     return (
       <div>
+        <div className="leaderboard-container">
+          <h2>Leaderboard</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>Score</th>
+                <th>Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.leaderboard.map((entry, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{entry.score}</td>
+                  <td>{`${entry.user.firstname} ${entry.user.lastname}`}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
         <div className="score-container">
           <div className="score">
             <span style={{ color: "white", fontWeight: "bold" }}>Score:</span>{" "}
